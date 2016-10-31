973537e7b4aa1cac0967130a438bcaff00c11556
akshay-sridhar <akshays.sridhar@gmail.com>
2016-10-31 16:11:47 +0000

    third commit

--- matplotlibplot.py
+++ matplotlibplot.py
@@ -1,6 +1,81 @@
+try:
+	import numpy as np 
+except:
+	print('Error: Unable to access the numpy module\n')
+	sys.exit(1)
+
+try:
+	import matplotlib.pyplot as plt
+except:
+	print('Error: Unable to access the matplotlib module\n')
+	sys.exit(1)
+
 import argparse
 import os
 import sys
+
+
+def cylinder():
+	'''
+	
+    Returns the unit cylinder that corresponds to the curve r.
+    INPUTS:  ro - a vector of radii (Default = 3.8)
+             n - number of coordinates to return for each element in r (Default = 40)
+
+    OUTPUTS: x,y,z - coordinates of points
+    '''
+
+    ro = 4.8
+    n = 40
+
+    points = np.linspace(0,2*np.pi,n+1)
+    x = np.atleast_2d((ro-1) * np.cos(points))
+    y = np.atleast_2d((ro-1) * np.sin(points))
+
+    x = np.concactenate((x, x), axis = 1)
+    y = np.concactenate((y, y), axis = 1)
+
+    z = np.zeros((2,n+1), dtype = float)
+    z[1,:] = z[1,:] + 1.0
+
+    X = np.concactenate(((np.zeros(1,41, dtype = float)), x, (np.zeros(1,41, dtype = float))), axis = 1)
+    Y = np.concactenate(((np.zeros(1,41, dtype = float)), y, (np.zeros(1,41, dtype = float))), axis = 1)
+    Z = np.concactenate(((np.zeros(1,41, dtype = float)), z, (np.ones(1,41, dtype = float))), axis = 1)
+
+    Z = (ro*Z) - (ro/2)
+
+    return X,Y,Z
+
+def wrapper_dna_coods(r, core_x, core_y, core_z):
+
+	ro = 4.8
+	d1 = 1.8
+
+	theta_0 = 0.6 * np.pi
+	thetas = np.linspace(theta_0, np.pi, num = 171, retstep = False, endpoint = True)
+
+	Z = d1 - ((thetas - theta_0) * ((2*d1)/((4*np.pi) - theta_0)))
+	X = ro * np.cos(thetas - (np.pi/2))
+	Y = ro * np.sin(thetas - (np.pi/2))
+
+	Xn = r[0] + (core_x[1]*X) + (core_y[1]*Y) + (core_z[1]*Z)
+	Yn = r[1] + (core_x[2]*X) + (core_y[2]*Y) + (core_z[2]*Z)
+	Zn = r[2] + (core_x[3]*X) + (core_y[3]*Y) + (core_z[3]*Z)
+
+	return Xn,Yn,Zn
+
+
+def nucleosome_cylinder(r,core_x,core_y,core_z):
+	X, Y, Z = cylinder()
+
+	Xc = r[0] + (core_x[1]*X) + (core_y[1]*Y) + (core_z[1]*Z)
+	Yc = r[1] + (core_x[2]*X) + (core_y[2]*Y) + (core_z[2]*Z)
+	Zc = r[2] + (core_x[3]*X) + (core_y[3]*Y) + (core_z[3]*Z)
+
+	return Xc,Yc,Zc
+
+
+
 
 parser = argparse.ArgumentParser(prog = 'CG Chromatin Visualizer', add_help = False,  formatter_class = argparse.RawDescriptionHelpFormatter, description =\
 '''
@@ -18,6 +93,7 @@ Option 				Description
 
 Optional options
 -------------------------------------------------------------------------
+-fc 			: The index of the first core to be plotted. DEFAULT = 1
 -lh				: Linker histone present (Y/N). DEFAULT = Y
 -plh				: Plot linker histone (Y/N). DEFAULT = Y
 -fr 				: File with frame numbers to be rendered 
@@ -31,6 +107,7 @@ Other options
 parser.add_argument('-f', nargs = 1, dest = 'xyzfile', help = argparse.SUPPRESS, required = True)
 parser.add_argument('-p', nargs = 1, dest = 'histfile', help = argparse.SUPPRESS, required = True)
 
+parser.add_argument('-fc', nargs = 1, dest = 'first_core', default = 1, type = int, help = argparse.SUPPRESS)
 parser.add_argument('-lh', nargs = 1, dest = 'togglelinker', default = ['Y'], help = argparse.SUPPRESS)
 parser.add_argument('-plh', nargs = 1, dest = 'plotlinker', default = ['Y'], help = argparse.SUPPRESS)
 parser.add_argument('-fr', nargs = 1, dest = 'framefile', default = ['Auto'], help = argparse.SUPPRESS)
@@ -46,6 +123,7 @@ args.framefile = args.framefile[0]
 args.outputfile = args.outputfile[0]
 args.togglelinker = args.togglelinker[0]
 args.plotlinker = args.plotlinker[0]
+args.first_core = args.first_core[0]
 
 if not os.path.isfile(args.xyzfile):
 	print('Input XYZ coordinate file does not exist\n')
@@ -69,21 +147,6 @@ elif args.togglelinker == 'N':
 
 if args.plotlinker != 'Y' and args.plotlinker != 'N':
 	print('Only values of "Y" and "N" are allowed with option -plh\n')
-	sys.exit(1)
-
-###############################################################################################################
-#Now importing the required modules. Doing this later to allow help to display even if modules are unavailable#
-###############################################################################################################
-try:
-	import numpy as np 
-except:
-	print('Error: Unable to access the numpy module\n')
-	sys.exit(1)
-
-try:
-	import matplotlib.pyplot as plt
-except:
-	print('Error: Unable to access the matplotlib module\n')
 	sys.exit(1)
 
 histonedetails = np.genfromtxt(args.histfile, dtype = int, usecols = 0)
@@ -179,7 +242,6 @@ for i in xrange(0,printframe.size):
 		histonetail_coods[histonetail_index,:] = currentcoods[deltaZ+histonetail_index,:]
 
 
-
 	if args.togglelinker == 'Y':
 
 		deltaZ = deltaZ + n_histonetails
@@ -194,22 +256,35 @@ for i in xrange(0,printframe.size):
 			linker_histone_coods3[linker_histone_index,:] = currentcoods[deltaZ+(linker_histone_index*3)+2,:]
 
 
+	center = np.mean(core_coods, axis = 0)
+
+	links_sum = np.sum(histonedetails[1:args.first_core])
+	rem = args.first_core % 2
+
+	colorindex = np.asarray(np.matrix('0,0,1;0,1,0;1,1,0;1,0,0;1,1,1;0.8,0.2,0;0.251,0.8784,0.8157'))
+
+	for core_index in xrange(args.first_core-1,n_cores):
+
+		rem2 = (core_index+1) % 2
+
+		r = core_coods[core_index,:]
+		core_x = core_orientations[(core_index*3)+1,:]
+		core_y = core_orientations[(core_index*3)+2,:]
+		core_z = core_orientations[(core_index*3)+3,:]
+
+		Xc, Yc, Zc = nucleosome_cylinder(r, core_x, core_y, core_z)
+
+		if rem == rem2:
+			cc = 1
+		elif rem != rem2:
+			cc = 5
+
+		Xn, Yn, Zn = wrapper_dna_coods(r, core_x, core_y, core_z)
+
+		
 
 
-
-
-
-
-
-
-
-	
-
-
-
-
-
-
+		links_sum = links_sum + histonedetails[core_index+1]
 
 
 
